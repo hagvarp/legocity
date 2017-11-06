f218ba19ff2e42cc5f4527ea673b035606d49645
Kári Holm Johannesen <khj@hagstova.fo>
2017-11-06 12:17:22 +0000

    Lønargjaldingar + Løntakarar

--- js/work.js
+++ js/work.js
@@ -5,9 +5,9 @@ Date.prototype.getMonthText = function() {
 }
 
 
-
-
-
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
 
 
 //---------------------------------------------------------
@@ -72,6 +72,143 @@ Date.prototype.getMonthText = function() {
             }
 
 
+
+
+//---------------------------------------------------------
+//                       CHART 2
+//---------------------------------------------------------
+            var chart2;
+            var chart2Data;
+            var chart2_last_month;
+
+            //Chart 1 specific functions
+            function drawChart2() {
+                var jsonStatData = chart2Data;
+                //Extrat Data From JSON Stat
+                ds = JSONstat(jsonStatData).Dataset(0);
+
+                //Format Data
+                var data = [];
+               
+                m = ds.Dimension("month").Category(ds.Dimension("month").id[0]).label;
+                mon = m.replace("M", "-");
+                mon = new Date(mon);
+                $('#employeesmonth').append(mon.getMonthText() + " " +  mon.getFullYear());
+                arbeidsloys = ds.Data({ "month": ds.Dimension("month").id[0] }, false);
+                arbeidsloys = numberWithCommas(arbeidsloys);
+                $('#employeesnumber').append(arbeidsloys);
+
+
+            }
+
+            function loadDataAndBuildChart2() {
+                POST("https://statbank.hagstova.fo/api/v1/en/H2/AM/AM03/lont_ajtr_t.px", {
+                    "query": [
+                        {
+                            "code": "value mode",
+                            "selection": {
+                                "filter": "item",
+                                "values": [
+                                    "LT"
+                                ]
+                            }
+                        },
+                        {
+                            "code": "month",
+                            "selection": {
+                                "filter": "top",
+                                "values": [
+                                    "1"
+                                ]
+                            }
+                        },                  
+
+
+
+                    ],
+                    "response": {
+                        "format": "json-stat"
+                    }
+                }, function(data) {
+                    chart2Data = data;
+                    drawChart2();
+                });
+            }
+
+
+
+
+
+//---------------------------------------------------------
+//                       CHART 3
+//---------------------------------------------------------
+            var chart3;
+            var chart3Data;
+            var chart3_last_month;
+
+            //Chart 1 specific functions
+            function drawChart3() {
+                var jsonStatData = chart3Data;
+                //Extrat Data From JSON Stat
+                ds = JSONstat(jsonStatData).Dataset(0);
+
+                //Format Data
+                var data = [];
+               
+                m = ds.Dimension("month").Category(ds.Dimension("month").id[0]).label;
+                mon = m.replace("M", "-");
+                mon = new Date(mon);
+                $('#wageearningmonth').append(mon.getMonthText() + " " +  mon.getFullYear());
+                arbeidsloys = ds.Data({ "month": ds.Dimension("month").id[0] }, false);
+                arbeidsloys = parseFloat(Math.round(arbeidsloys * 100) / 100000).toFixed(0).toString().replace(".", ",");
+                $('#wageearningnumber').append(arbeidsloys);
+
+
+            }
+
+            function loadDataAndBuildChart3() {
+                POST("https://statbank.hagstova.fo/api/v1/fo/H2/AM/AM04/lon_ajtrmd_t.px", {
+                    "query": [
+                        {
+                            "code": "value mode",
+                            "selection": {
+                                "filter": "item",
+                                "values": [
+                                    "WA"
+                                ]
+                            }
+                        },
+                        {
+                            "code": "month",
+                            "selection": {
+                                "filter": "top",
+                                "values": [
+                                    "1"
+                                ]
+                            }
+                        },                  
+
+
+
+                    ],
+                    "response": {
+                        "format": "json-stat"
+                    }
+                }, function(data) {
+                    chart3Data = data;
+                    drawChart3();
+                });
+            }
+
+
+
+
+
+
+
+
+
+
             //HELPER FUNCTION
             function POST(url, query, main) {
                 $.ajax({
@@ -87,5 +224,6 @@ Date.prototype.getMonthText = function() {
             //START LOADING AND BUILDING CHARTS
             $(function () {
                 loadDataAndBuildChart1();
+                loadDataAndBuildChart2();
+                loadDataAndBuildChart3();
             });            
-
